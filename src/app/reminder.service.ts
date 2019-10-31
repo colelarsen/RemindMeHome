2728bc92077f8242affc6c8b07b2e220e14fea8c
colelarsen <larsenca@appstate.edu>
2019-10-31 11:24:12 -0400

    reminders update

--- src/app/reminder.service.ts
+++ src/app/reminder.service.ts
@@ -10,18 +10,18 @@ export class ReminderService {
   constructor(private http: HttpClient) { }
 
   public getReminders() {
-    return this.http.get<{string: Reminder}>("http://remindmehome.com/reminders");
+    return this.http.get<{string: Reminder}>("http://remindmehome.com/reminders/");
   }
 
   public deleteReminder(reminder:Reminder) {
-    return this.http.post("http://remindmehome.com/reminders/delete", reminder);
+    return this.http.post("http://remindmehome.com/reminders/", reminder);
   }
 
   public addReminder(reminder:Reminder) {
-    return this.http.post("http://remindmehome.com/reminders", reminder);
+    return this.http.post("http://remindmehome.com/reminders/", reminder);
   }
 
   public updateReminder(reminder:Reminder){
-    return this.http.put("http://remindmehome.com/reminders", reminder);
+    return this.http.put("http://remindmehome.com/reminders/", reminder);
   }
 }
