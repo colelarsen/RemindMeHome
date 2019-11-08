caca19d884af5ea31e962134bc90dad384e5da6b
cole.larsen <cole.larsen@infosys.com>
2019-11-08 12:24:21 -0500

    changed service to public

--- src/app/reminder.service.ts
+++ src/app/reminder.service.ts
@@ -12,8 +12,8 @@ export class ReminderService {
   {
     const publicWeb = "http://remindmehome.com/reminders/";
     const localWeb = "http://localhost:3050/";
-    return localWeb;
-    // return publicWeb;
+    // return localWeb;
+    return publicWeb;
   }
   
 
